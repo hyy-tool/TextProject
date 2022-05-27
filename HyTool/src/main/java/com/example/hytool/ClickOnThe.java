1ffe11afde59f8bdc42b5d18d8eae76efae04031
XTZJ-20220310PP\Administrator <626091080@qq.com>
2022-05-27 09:13:09 +0800

    no message

---
+++ HyTool/src/main/java/com/example/hytool/ClickOnThe.java
@@ -0,0 +1,15 @@
+package com.example.hytool;
+
+import android.widget.Toast;
+
+/**
+ * @Author : HYY
+ * 描述:
+ * 时间:      2022/05/2022/5/27
+ * 软件包名:   com.example.hytool
+ */
+public interface ClickOnThe {
+    public static void dianji() {
+
+    }
+}
--- app/src/main/java/com/example/textproject/MainActivity.java
+++ app/src/main/java/com/example/textproject/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hytool.ClickOnThe;
+
+public class MainActivity extends AppCompatActivity implements ClickOnThe {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
